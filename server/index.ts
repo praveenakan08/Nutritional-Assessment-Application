640058798f7ca2ba98535fd938092ebfd1fb51f0
Mansi Rathi <Mansi.Rathi0@experian.com>
2023-11-25 02:00:42 -0600

    fix:updated build folder

--- server/index.ts
+++ server/index.ts
@@ -18,14 +18,14 @@ const corsOptions = {
 };
 console.log("UI Build", UI_BUILD);
 
-// app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 // console.log(path.join(__dirname, "./client/build"));
 app.use(fileUpload());
 app.use(express.json());
 app.use(cors(corsOptions));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
