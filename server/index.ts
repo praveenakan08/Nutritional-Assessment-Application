3fd20bbe20abb82ed16a8c26e63046ea0f4a900d
Mansi Rathi <Mansi.Rathi0@experian.com>
2023-11-27 19:12:12 -0600

    fix:changed App name and icon displayed in the browser

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { RegisterModel, MetricModel } from "./models";
 import path, { join } from "path";
 import * as tfn from "@tensorflow/tfjs-node";
+import * as tf from "@tensorflow/tfjs";
 import { Request, Response } from "express";
 import fileUpload, { UploadedFile } from "express-fileupload";
 import fs from "fs";
@@ -203,13 +204,16 @@ app.post("/api/analyze", async (req: Request, res: Response) => {
     }
     const image = req.files.files as UploadedFile;
     const email = req.body.email;
-    const handler = tfn.io.fileSystem("./ml_model/model.json");
-    const model = await tfn.loadGraphModel(handler);
+    const modelPath =
+      "file://" + path.resolve(__dirname, "ml_model/model.json");
+    //const handler = tfn.io.fileSystem("./ml_model/model-5");
+    const model = await tfn.loadGraphModel(modelPath);
 
     const imgBuffer = Buffer.from(image.data);
     const imgTensor = tfn.node.decodeImage(imgBuffer);
 
     const resizedImgTensor = tfn.image.resizeBilinear(imgTensor, [224, 224]);
+
     const channels = 3;
 
     const normalizedImgTensor = resizedImgTensor.toFloat().div(255);
